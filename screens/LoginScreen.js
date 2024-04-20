8e05f12c49f4705de2256a143909603d51b56493
Benny (YC0SPU) <chandra@benny.id>
2024-04-20 09:54:04 +0700

    Add basic structure to LoginForm

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Image, Button, Alert } from 'react-native';
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function LoginScreen() {
   const insets = useSafeAreaInsets();
+  const [username, onChangeUsername] = React.useState('Username Input');
+  const [password, onChangePassword] = React.useState('Password Input');
   return (
     <View style={{ flex: 1, paddingTop: insets.top }}>
-      <Text>This is Login Screen</Text>
+      <Image
+        source={require('../assets/CoffeeStashLogo1.png')}
+      />
+      <TextInput
+        onChangeText={onChangeUsername}
+        value={username}
+      />
+      <TextInput
+        onChangeText={onChangePassword}
+        value={password}
+      />
+      <Button
+        onPress={() => Alert.alert('Button with adjusted color pressed')}
+        title="Sign In"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </View>
   );
 }
