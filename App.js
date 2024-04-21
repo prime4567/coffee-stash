07d639468c6eff1f16d374f206c7c98125fb9555
Benny (YC0SPU) <chandra@benny.id>
2024-04-21 11:07:40 +0700

    Add Stack Navigator to Login Screen

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { PaperProvider, MD3LightTheme as DefaultTheme, } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Import Screen
 import LoginScreen from './screens/LoginScreen'
 import MyStashScreen from './screens/MyStashScreen';
 
@@ -15,12 +19,18 @@ const BrownTheme = {
   },
 };
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <SafeAreaProvider>
       <PaperProvider theme={BrownTheme}>
-        <MyStashScreen />
-        <StatusBar style="light" />
+        <NavigationContainer theme={BrownTheme}>
+          <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name="SignInScreen" component={LoginScreen} />
+            <Stack.Screen name="MyStashScreen" component={MyStashScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </PaperProvider>
     </SafeAreaProvider>  );
 }
